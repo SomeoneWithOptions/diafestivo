ebd490436eb30b1bb8f10c007e680674f63ba3ff
SomeoneWithOptions <andrescastesol@gmail.com>
2023-04-04 12:17:59 -0500

    added description and celebration rendering

--- script.js
+++ script.js
@@ -7,30 +7,36 @@ fetch(HOLIDAY_API)
     const mainDiv = document.querySelector(".main-container");
     const isHoliday = data.nextHoliday.isToday;
     const daysRemaining = data.nextHoliday.daysUntil;
+    const nextDate = data.nextHoliday.date;
+    const celebrationName = data.nextHoliday.name;
+
     renderTitle(mainDiv);
     renderIsHolidayText(mainDiv, isHoliday);
     renderDaysRemaining(mainDiv, isHoliday, daysRemaining);
+    renderDescription(mainDiv, isHoliday, nextDate);
+    renderCelebration(mainDiv, celebrationName);
   })
   .catch((e) => console.log(e));
 
 function hideLoading() {
   document.querySelector(".loading").style.display = "none";
 }
-
-function renderIsHolidayText(div, isHoliday) {
-  const pTag = document.createElement("p");
-  pTag.textContent = isHoliday ? "SI!" : "NO :(";
-  pTag.classList.add("normalText");
-  div.appendChild(pTag);
-}
-
 function renderTitle(div) {
   const h1Tag = document.createElement("h1");
   h1Tag.textContent = "¿Es Hoy Festivo en Colombia?";
   div.appendChild(h1Tag);
 }
 
-function renderDaysRemaining(div, isHoliday, days) {
+function renderIsHolidayText(div, isHoliday)
+{
+  const pTag = document.createElement("p");
+  pTag.textContent = isHoliday ? "SI!" : "NO :(";
+  pTag.classList.add("normalText");
+  div.appendChild(pTag);
+}
+
+function renderDaysRemaining(div, isHoliday, days)
+{
   if (isHoliday) {
     return;
   }
@@ -42,4 +48,59 @@ function renderDaysRemaining(div, isHoliday, days) {
   div.appendChild(remainingDays);
 }
 
-function renderDescription(div, isHoliday) {}
+function formatDateToText(dateStr) {
+  const weekdays = [
+    "Domingo",
+    "Lunes",
+    "Martes",
+    "Miércoles",
+    "Jueves",
+    "Viernes",
+    "Sábado",
+  ];
+  const months = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre",
+  ];
+
+  const date = new Date(dateStr);
+
+  console.log(date);
+  const dayOfWeek = weekdays[date.getUTCDay()];
+  const dayOfMonth = date.getUTCDate();
+  const month = months[date.getUTCMonth()];
+  const year = date.getUTCFullYear();
+
+  return `${dayOfWeek}, ${dayOfMonth} de ${month} de ${year}`;
+}
+
+function renderDescription(div, isHoliday, festiveDate) {
+  if (isHoliday) {
+    return;
+  }
+  const description = document.createElement("p");
+  description.textContent = `El proximo festivo es el dia ${formatDateToText(
+    festiveDate
+  )}`;
+  description.classList.add("normalText");
+  div.appendChild(description);
+}
+
+function renderCelebration(div, celebrationName, isHoliday) {
+  const celebration = document.createElement("p");
+  celebration.textContent = ` ${
+    isHoliday ? "Celebramos" : "Celebraremos"
+  } ${celebrationName}`;
+  celebration.classList.add("normalText");
+  div.appendChild(celebration);
+}
