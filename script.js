f047d64c728f1cf3944ef669a837d3aa7b8c6d84
SomeoneWithOptions <andrescastesol@gmail.com>
2023-04-04 11:34:12 -0500

    change how elements are rendered after fetching the api

--- script.js
+++ script.js
@@ -3,10 +3,27 @@ const HOLIDAY_API = "https://holiday.sanetomore.com/";
 fetch(HOLIDAY_API)
   .then((r) => r.json())
   .then((data) => {
-    setIsHolidayText(data.nextHoliday.isToday);
+    hideLoading();
+    const mainDiv = document.querySelector(".main-container");
+    renderTitle(mainDiv);
+    renderIsHolidayText(mainDiv,data.nextHoliday.isToday);
   })
   .catch((e) => console.log(e));
 
-  function setIsHolidayText(isHoliday) {
-    document.querySelector(".is-holiday").textContent = isHoliday ? "SI!" : "NO :(";
-  }
+function renderIsHolidayText(div ,isHoliday) {
+  const pTag = document.createElement("p");
+  pTag.textContent = isHoliday ? "SI!" : "NO :(";
+  pTag.classList.add('is-holiday')
+  div.appendChild(pTag)
+}
+
+function renderTitle(div) {
+  const h1Tag = document.createElement("h1");
+  h1Tag.textContent = "¿Es Hoy Festivo en Colombia?";
+  div.appendChild(h1Tag);
+}
+
+function hideLoading() {
+  document.querySelector(".loading").style.display = "none";
+}
+
