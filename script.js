d49cbf3a60b7a3fcb9e1658581528d532314d954
SomeoneWithOptions <andrescastesol@gmail.com>
2023-04-04 15:12:16 -0500

    refactor css and js

--- script.js
+++ script.js
@@ -4,37 +4,38 @@ fetch(HOLIDAY_API)
   .then((r) => r.json())
   .then((data) => {
     hideLoading();
-    const mainDiv = document.querySelector(".main-container");
+    const mainContainer = document.querySelector(".main-container");
     const isHoliday = data.nextHoliday.isToday;
     const daysRemaining = data.nextHoliday.daysUntil;
     const nextDate = data.nextHoliday.date;
     const celebrationName = data.nextHoliday.name;
 
-    renderTitle(mainDiv);
-    renderIsHolidayText(mainDiv, isHoliday);
-    renderDaysRemaining(mainDiv, isHoliday, daysRemaining);
-    renderDescription(mainDiv, isHoliday, nextDate);
-    renderCelebration(mainDiv, celebrationName);
+    renderTitle(mainContainer);
+    renderIsHolidayText(mainContainer, isHoliday);
+    renderDaysRemaining(mainContainer, isHoliday, daysRemaining);
+    renderDescription(mainContainer, isHoliday, nextDate);
+    renderCelebration(mainContainer, celebrationName);
   })
   .catch((e) => console.log(e));
 
 function hideLoading() {
   document.querySelector(".loading").style.display = "none";
 }
+
 function renderTitle(div) {
+  const title = "¿Es Hoy Festivo en Colombia?";
   const titleTag = document.createElement("p");
-  titleTag.textContent = "¿Es Hoy Festivo en Colombia?";
-  titleTag.classList.add("title");
-  titleTag.classList.add("yellow");
+  titleTag.textContent = title;
+  titleTag.classList.add("title", "yellow");
   div.appendChild(titleTag);
 }
 
 function renderIsHolidayText(div, isHoliday) {
-  const pTag = document.createElement("p");
-  pTag.textContent = isHoliday ? "SI!" : "NO :(";
-  pTag.classList.add("normalText");
-  pTag.classList.add("blue");
-  div.appendChild(pTag);
+  const text = isHoliday ? "SI!" : "NO :(";
+  const isHolidayTag = document.createElement("p");
+  isHolidayTag.textContent = text;
+  isHolidayTag.classList.add("normalText", "blue");
+  div.appendChild(isHolidayTag);
 }
 
 function renderDaysRemaining(div, isHoliday, days) {
@@ -47,6 +48,27 @@ function renderDaysRemaining(div, isHoliday, days) {
   }`;
   remainingDays.classList.add("thin");
   div.appendChild(remainingDays);
+}
+
+function renderDescription(div, isHoliday, festiveDate) {
+  if (isHoliday) {
+    return;
+  }
+  const description = document.createElement("p");
+  description.textContent = `El próximo festivo es el dia ${formatDateToText(
+    festiveDate
+  )}`;
+  description.classList.add("thin");
+  div.appendChild(description);
+}
+
+function renderCelebration(div, celebrationName, isHoliday) {
+  const celebration = document.createElement("p");
+  celebration.textContent = ` ${
+    isHoliday ? "Celebramos" : "Celebraremos"
+  } ${celebrationName}`;
+  celebration.classList.add("normalText", "red");
+  div.appendChild(celebration);
 }
 
 function formatDateToText(dateStr) {
@@ -76,33 +98,10 @@ function formatDateToText(dateStr) {
 
   const date = new Date(dateStr);
 
-  console.log(date);
   const dayOfWeek = weekdays[date.getUTCDay()];
   const dayOfMonth = date.getUTCDate();
   const month = months[date.getUTCMonth()];
   const year = date.getUTCFullYear();
 
   return `${dayOfWeek}, ${dayOfMonth} de ${month} de ${year}`;
-}
-
-function renderDescription(div, isHoliday, festiveDate) {
-  if (isHoliday) {
-    return;
-  }
-  const description = document.createElement("p");
-  description.textContent = `El próximo festivo es el dia ${formatDateToText(
-    festiveDate
-  )}`;
-  description.classList.add("thin");
-  div.appendChild(description);
-}
-
-function renderCelebration(div, celebrationName, isHoliday) {
-  const celebration = document.createElement("p");
-  celebration.textContent = ` ${
-    isHoliday ? "Celebramos" : "Celebraremos"
-  } ${celebrationName}`;
-  celebration.classList.add("normalText");
-  celebration.classList.add("red");
-  div.appendChild(celebration);
 }
